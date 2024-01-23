c8084e8048e88dfbd09857722571e11cf256cb89
hazemafara <566508@edu.rocmn.nl>
2024-01-24 00:19:55 +0100

    final commit

--- app/models/InstructeurModel.php
+++ app/models/InstructeurModel.php
@@ -262,5 +262,16 @@ class InstructeurModel
         $this->db->single();
     }
 
+    function deleteInstructeur($instructeurId)
+    {
+        $sql = "delete from VoertuigInstructeur where InstructeurId = ?";
+        $this->db->query($sql);
+        $this->db->bind(1, $instructeurId);
+        $this->db->single();
 
+        $sql = "delete from Instructeur where Id = ?";
+        $this->db->query($sql);
+        $this->db->bind(1, $instructeurId);
+        $this->db->single();
+    }
 }
