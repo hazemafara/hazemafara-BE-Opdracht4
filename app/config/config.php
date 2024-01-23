351049fdb11c76fa625f2b0d5ba7d47d485fa01d
hazemafara <566508@edu.rocmn.nl>
2024-01-24 00:12:58 +0100

    inital commit

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'backend-opdracht-4');
+define('URLROOT', 'http://backend-opdracht-4.test');
